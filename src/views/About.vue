6bcaf276c4475508dcfe20a49585fc81f6bdcf4a
Matthew Caloger <mattcaloger@gmail.com>
2019-08-30 09:16:51 -0400

    Update about.vue, resolves issue #12

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This project is maintained by the following people...</h1>
+
+    <div>
+      <a href="https://github.com/mcaloger"><h2>Matthew Caloger</h2></a>
+      <p>Project Manager & Full-Stack Developer</p>      
+    </div>
   </div>
 </template>
