1f38a63170055c96cab0b9a0d79d1e6879b77d58
DragonWraithus <none@unknown.localhost>
2019-08-30 20:54:57 -0600

    added github profile to About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This project is maintained by the following people...</h1>
-
-    <div>
-      <a href="https://github.com/mcaloger"><h2>Matthew Caloger</h2></a>
-      <p>Project Manager & Full-Stack Developer</p>      
-    </div>
-  </div>
+	<div class="about">
+		<h1>This project is maintained by the following people...</h1>
+		<div>
+			<a href="https://github.com/mcaloger"><h2>Matthew Caloger</h2></a>
+			<p>Project Manager & Full-Stack Developer</p>      
+		</div>
+		
+		<div>
+			<a href="https://github.com/DragonWraithus"><h2>Dragon Wraithus</h2></a>
+			<p>Peon & Rust Developer</p>
+		</div>
+	</div>
 </template>
