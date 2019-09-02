e44b5ce2885a53f76fa09efc3542ac6f35fed468
flyingsl0ths <randomemail32110@gmail.com>
2019-09-01 22:50:24 -0500

    empty commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,11 +7,10 @@
       <p>Project Manager & Full-Stack Developer</p>
     </div>
 
-    <div>
+    <div class ="sloth">
         <br><br>
         <a href="https://github.com/flyingsl0ths"><h2>FlyingSloths</h2></a>
         <p style="font-style: italic;">Python & JavaScript Developer</p>
     </div>
-
   </div>
 </template>
