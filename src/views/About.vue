6fb46d43c0df599d2082ffb27f4e2e2a405508e8
flyingsl0ths <randomemail32110@gmail.com>
2019-09-12 02:45:38 -0500

    Removed CSS

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,9 +3,9 @@
 		<h1>This project is maintained by the following people...</h1>
 		<div>
 			<a href="https://github.com/mcaloger"><h2>Matthew Caloger</h2></a>
-			<p>Project Manager & Full-Stack Developer</p>      
+			<p>Project Manager & Full-Stack Developer</p>
 		</div>
-		
+
 		<div>
 			<a href="https://github.com/DragonWraithus"><h2>Dragon Wraithus</h2></a>
 			<p>Peon & Rust Developer</p>
@@ -13,7 +13,12 @@
 
 		<div>
 			<a href="https://github.com/J-Garcia-CPP"><h2>Josue Garcia</h2></a>
-			<p>Java Dev</p>      
+			<p>Java Dev</p>
 		</div>
+
+        <div>
+            <a href="https://github.com/flyingsl0ths"><h2>Flying Sloths</h2></a>
+            <p>Python Developer</p>
+        </div>
 	</div>
 </template>
